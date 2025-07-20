0b5f2321a0f48f79d2c7985b2e4ecb188c190ec9
Arthur Jackson <jacksonal@cua.edu>
2025-07-20 11:33:28 -0400

    Added code for quad pump controller

---
+++ tests/publish_to_quad_pump.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+def publish_pump_state(broker_ip, message):
+    """
+    Publish b'0' or b'1' to topic 'waterer_pump/2'.
+
+    Args:
+        broker_ip (str): IP address of the MQTT broker.
+        message (bytes): Must be b'0' or b'1'.
+    """
+    if message not in [b'0', b'1']:
+        raise ValueError("message must be b'0' or b'1'")
+
+    client = mqtt.Client()
+    client.connect(broker_ip, 1883, 60)
+    client.publish("waterer_pump/2", message)
+    client.disconnect()
+
+    publish_pump_state("192.168.1.100", b'1')
