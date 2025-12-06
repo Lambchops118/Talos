e09f35c69d8a93e0fc35dcabe9a9275219b79523
Lambchops118 <aljackson118@gmail.com>
2025-12-05 20:04:45 -0500

    went back to google SR from openai whisper

--- InfoPanel/screen.py
+++ InfoPanel/screen.py
@@ -142,7 +142,7 @@ def static_drawings(screen, base_w, base_h, scale_x, scale_y, circle_time):
     #print(tasks.bitcoin_price)
 
     draw_text_centered("[Weather Forecast]", base_w/4.5, (base_h/14)+150,  color, 40)
-    draw_text_centered(f"Bitcoin Price {tasks.bitcoin_price}",     base_w/4.5, (base_h/14)+200,  color, 40)
+    draw_text_centered(f"BTC: ${tasks.bitcoin_price} | ETH: ${tasks.ethereum_price}, | SOL: ${tasks.solana_price}",     base_w/4.5, (base_h/14)+200,  color, 40)
     draw_text_centered("[Fear Greed Index]", base_w/4.5, (base_h/14)+250,  color, 40)
     draw_text_centered("[Something Else]",   base_w/4.5, (base_h/14)+300,  color, 40)
 
